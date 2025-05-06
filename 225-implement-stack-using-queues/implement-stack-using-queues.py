5d4e4a29e8129089d3dfa55b2a63c527c4ba90c7
Xavier Jean Baptiste <31086996+xavierpjb@users.noreply.github.com>
2025-05-05 20:22:29 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (90.02%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.py
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -21,35 +21,31 @@ from collections import deque
 class MyStack:
 
     def __init__(self):
-        self.q1 = deque()
-        self.q2 = deque()
+        self.q = deque()
         
 
     def push(self, x: int) -> None:
-        self.q1.append(x)
+        self.q.append(x)
         
 
     def pop(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
 
-        ans = self.q1.popleft()
-        self.q1, self.q2 = self.q2, self.q1
-        return ans
+        return self.q.popleft()
         
 
     def top(self) -> int:
-        while len(self.q1) > 1:
-            self.q2.append(self.q1.popleft())
-
-        ans = self.q1.popleft()
-        self.q2.append(ans)
-        self.q1, self.q2 = self.q2, self.q1
+        for i in range(len(self.q) - 1):
+            self.q.append(self.q.popleft())
+        
+        ans = self.q.popleft()
+        self.q.append(ans)
         return ans
         
 
     def empty(self) -> bool:
-        return len(self.q1) == 0
+        return len(self.q) == 0
         
 
 
