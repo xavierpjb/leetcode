0ceec55247f71e25f438ef85eca16a41ed92ab74
Xavier Jean Baptiste <31086996+xavierpjb@users.noreply.github.com>
2025-09-02 19:47:39 -0400

    Time: 725 ms (73.76%) | Memory: 18.9 MB (31.82%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,38 +1,30 @@
-'''
-Problem return min num coins
-
-look for min nums coins to reach 0
-
-base cases:
-
-min number of coins to reach denom is 1
-if
-
-brute force
-
-try all possible combination of coins that reach amount, return the one with min coins
-
-f(amount) = 1 + min(f(amount-coin) for coin in coins)
-impossible paths should return -1
-'''
+from collections import deque
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        memo = {0: 0}
-        return self.rec(amount, memo, coins)
-        
+        if amount == 0:
+            return 0
+        q = deque(coins)
+        visited = set()
+        level = 1
+        lq = len(q)
+        while q:
+            curr = q.popleft()
+            if curr == amount:
+                return level
+            lq -= 1
+            
+            if curr < amount and curr not in visited:
+                visited.add(curr)
 
-    def rec(self, amount, memo, coins):
-        if amount in memo:
-            return memo[amount]
-        if amount < 0:
-            return -1
-        
-        opts = [self.rec(amount - coin, memo, coins) for coin in coins ]
-        opts = list(filter(lambda x: x != -1, opts))
+                for nei in [curr + coin for coin in coins]:
+                    q.append(nei)
 
+            if lq == 0:
+                lq = len(q)
+                level += 1
+            
 
-        memo[amount] = (1 + min(opts)) if len(opts) > 0 else -1
+        return -1
 
-        return memo[amount]
 
         
