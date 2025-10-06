3eb322acb4db07ba0027d46c96dd10a52625ecf7
Xavier Jean Baptiste <31086996+xavierpjb@users.noreply.github.com>
2025-10-06 19:36:36 -0400

    Time: 3 ms (63.56%) | Memory: 17.7 MB (97.19%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,49 @@
+'''
+in linear time we can tell whether a k is valid
+n log sum off all elements
+
+such that all sums less than target sum
+'''
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        ans = 0
+        def isValidWithMaxSum(maxSum):
+            # create arrays of at most size elements and
+            numSubArr = 1
+            currSum = 0
+            for num in nums:
+                if num > maxSum:
+                    return False
+                if currSum + num > maxSum:
+                    numSubArr += 1
+                    if numSubArr > k:
+                        return False
+                    currSum = num
+                else:
+                    currSum += num
+                     
+            return True
+
+        l, r = max(nums), sum(nums)
+        while l < r:
+            mid = (l+r) // 2
+            if isValidWithMaxSum(mid):
+                r = mid
+            else:
+                l = mid + 1
+        ans = 0
+        currSum = 0
+        for num in nums:
+            if currSum + num > l:
+                currSum = num
+            else:
+                currSum += num
+            ans = max(ans,currSum)
+
+
+
+        return ans
+
+
+
+        
