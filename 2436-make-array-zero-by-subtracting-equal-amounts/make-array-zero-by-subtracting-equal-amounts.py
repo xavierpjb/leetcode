335b7dcc1f0656b2f382d704948e2a30ad7a45f1
Xavier Jean Baptiste <31086996+xavierpjb@users.noreply.github.com>
2025-10-07 18:25:05 -0400

    Time: 0 ms (100.00%) | Memory: 17.7 MB (82.23%) - LeetSync

--- 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
+++ 2436-make-array-zero-by-subtracting-equal-amounts/make-array-zero-by-subtracting-equal-amounts.py
@@ -5,14 +5,6 @@ insert all nums into a pq, subtract then repeat
 class Solution:
     def minimumOperations(self, nums: List[int]) -> int:
         uniqNums = set(nums)
-        numZ = 0
-        for num in nums:
-            if num == 0:
-                numZ += 1
-
-        if len(nums) == numZ:
-            return 0
-
         ans = len(uniqNums)
         if 0 in uniqNums:
             ans -= 1
